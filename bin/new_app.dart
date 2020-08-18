836f231921ea476f981781513a86999e4cee8b8f
Evandro Barbosa <vandomarinho.vm@gmail.com>
2020-08-18 19:10:52 -0300

    discomentando o codigo

--- bin/new_app.dart
+++ bin/new_app.dart
@@ -8,53 +8,52 @@ void main(List<String> arguments) async {
   // Access a collection
   var coll = db.collection('people');
 
-  // TODO Read people
-  // var people = await coll.find().toList();
-  // print(people);
+  // Read people
+  var people = await coll.find().toList();
+  print(people);
 
-  // var people1 = await coll.find(where.limit(5)).toList();
-  // print(people1);
+  var people1 = await coll.find(where.limit(5)).toList();
+  print(people1);
 
   //Search for a person in the database
-  // var person = await coll.find(where.eq('first_name', 'Bruna'));
-  // print(person);
+  var person = await coll.find(where.eq('first_name', 'Bruna'));
+  print(person);
 
-  // var person1 = await coll.findOne(where.match('first_name', 'B'));
-  // print(person1);
+  var person1 = await coll.findOne(where.match('first_name', 'B'));
+  print(person1);
 
-  // var person1 =
-  //     await coll.findOne(where.match('first_name', 'B').and(where.gt('id', 6)));
-  //     .findOne(where.match('first_name', 'B').gt('id', 6));
-  //     .findOne(where.jsQuery('''
-  //     return this.first_name.startsWith('B') && this.id > 6;
-  // '''));
-  // print(person1);
+  var person2 = await coll
+      // .findOne(where.match('first_name', 'B').and(where.gt('id', 6)));
+      // .findOne(where.match('first_name', 'B').gt('id', 6));
+      .findOne(where.jsQuery('''
+      return this.first_name.startsWith('B') && this.id > 6;
+  '''));
+  print(person2);
 
   // Create person
 
-  // await coll.save({
-  //   'id': 12,
-  //   'first_name': 'Evandro',
-  //   'last_name': 'Barbosa',
-  //   'email': 'evandro@gmail.com',
-  //   'gender': 'Male'
-  // });
+  await coll.save({
+    'id': 12,
+    'first_name': 'Evandro',
+    'last_name': 'Barbosa',
+    'email': 'evandro@gmail.com',
+    'gender': 'Male'
+  });
 
-  // print('Saved new person');
+  print('Saved new person');
 
   // Update person
-  // await coll
-  //     .update(await coll.findOne(where.eq('id', 1)), {
-  //   r'$set': {'gender': 'Female'}
-  // });
+  await coll.update(await coll.findOne(where.eq('id', 1)), {
+    r'$set': {'gender': 'Female'}
+  });
 
-  // print('Updated person');
-  // print(await coll.findOne(where.eq('id', 8)));
+  print('Updated person');
+  print(await coll.findOne(where.eq('id', 8)));
 
   // Delete person
-  // await coll.remove(await coll.findOne(where.eq('id', 8)));
+  await coll.remove(await coll.findOne(where.eq('id', 8)));
 
-  // print('Delete pernso  ${await coll.findOne(where.eq('id', 8))}');
+  print('Delete pernso  ${await coll.findOne(where.eq('id', 8))}');
 
   await db.close();
 }
